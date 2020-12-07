3c8d73e83988432cab6b8c84ae0a430f829a1b59
charmiidalal <dalal.c@northeastern.edu>
2020-12-07 18:06:21 -0500

    Inspector integration with ent

--- src/Business/Role/InspectorRole.java
+++ src/Business/Role/InspectorRole.java
@@ -10,7 +10,7 @@ import Business.Network.Network;
 import Business.Organization.Organization;
 import Business.UserAccount.UserAccount;
 import javax.swing.JPanel;
-import userinterface.InspectorRole.InspectorWorkAreaJPanel1;
+import userinterface.InspectorRole.InspectorWorkAreaJPanel;
 
 /**
  *
@@ -20,7 +20,7 @@ public class InspectorRole extends Role{
 
     @Override
     public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise,Network network, EcoSystem business) {
-        return new InspectorWorkAreaJPanel1(userProcessContainer,business, account);
+        return new InspectorWorkAreaJPanel(userProcessContainer,account,organization,enterprise,network,business);
     }
 
     
